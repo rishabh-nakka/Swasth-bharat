1255b6bfa39ae179f783510e279edb9b612e1cc3
rishabh-nakka <76552521+rishabh-nakka@users.noreply.github.com>
2021-05-04 22:49:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show(){
+    window.location.href="https://www.google.com/search?rlz=1C1CHBF_enIN895IN895&tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALeKk02_BrUzfaBxs8-16LUXNI47LMhmpg:1620146686193&q=yoga+instructors+map&rflfq=1&num=10&ved=2ahUKEwiLl9vdvLDwAhVlIbcAHRRJD4YQtgN6BAgREAc#rlfi=hd:;si:;mv:[[17.294921877369937,78.59298706814207],[15.23317836723289,74.47311402126707],null,[16.26675603294228,76.53305054470457],9]";
+}
+function ambulance(){
+    window.location.href="https://www.google.com/search?rlz=1C1CHBF_enIN895IN895&tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALeKk03TLJrqSsm8pQTvYsSsQlRPEH05hA:1620147681218&q=nearest+ambulance&rflfq=1&num=10&ved=2ahUKEwiV55a4wLDwAhXz7XMBHSSaBREQtgN6BAgHEAc#rlfi=hd:;si:;mv:[[13.21347658685017,78.23415437128908],[12.690182612333015,77.20418610957033],null,[12.9519669968719,77.7191702404297],11]";
+}
